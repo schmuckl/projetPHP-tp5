32d9a4846bd96b72cdc4398cd6e6619e4e37ef31
SCHMUCK Leo <schmuckl@hostname.iut2.upmf-grenoble.fr>
2017-10-17 09:11:55 +0200

    Nouvelle.class.php

--- model/Nouvelle.class.php
+++ model/Nouvelle.class.php
@@ -29,11 +29,23 @@ class Nouvelle {
     return $this->urlImage;
   }
 
-  ...
+
+  function __construct(array $tab) {
+    $tab->titre = $titre;
+    $tab->date = $date;
+    $tab->description = $description;
+    $tab->url = $url;
+    $tab->urlImage = $urlImage;
+  }
 
   // Charge les attributs de la nouvelle avec les informations du noeud XML
   function update(DOMElement $item) {
-
+    $titre = $item->getElementsByTagName('title')->item(0)->textContent;
+    $date = $item->getElementsByTagName('pubDate')->item(0)->textContent;
+    $description = $item->getElementsByTagName('description')->item(0)->textContent;
+    $url = $item->getElementsByTagName('link')->item(0)->textContent;
+    $urlImage = $item->getElementsByTagName('enclosure');
   }
+}
 
 ?>
