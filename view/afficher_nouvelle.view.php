bb54c479b59b0ba3085794ed19ae04f93fe04c81
SCHMUCK Leo <schmuckl@hostname.iut2.upmf-grenoble.fr>
2017-10-30 12:38:42 +0100

    Plus fin CSS

--- view/afficher_nouvelle.view.php
+++ view/afficher_nouvelle.view.php
@@ -8,8 +8,8 @@
   </head>
   <body>
   <div class="nav-wrapper">
-    <a 
-      href="../controler/afficher_flux.ctrl.php" 
+    <a
+      href="../controler/afficher_flux.ctrl.php"
       class="nav-item">
       Retour à l'acceuil
     </a>
@@ -20,12 +20,19 @@
       <h6><?= $nouvelle->getDate() ?></h6>
     </div>
 
-    <p><?=$nouvelle->getDescription()?></p>
-    <!-- Si l'image n'est pas stockée dans la BD alors il n'y a pas d'image à afficher -->
-    <?php if (file_exists($nouvelle->getImage())) {?>
-      <img src="<?= $nouvelle->getImage()?>" alt="riplimage">
-    <?php } ?>
+    <div id="gauche">
+      <p><?=$nouvelle->getDescription()?></p><br>
+      <a class="nav-item2" href="<?= $nouvelle->getUrl()?>">
+        Article complet
+      </a>
+    </div>
+
+    <div id="droite">
+      <!-- Si l'image n'est pas stockée dans la BD alors il n'y a pas d'image à afficher -->
+      <?php if (file_exists($nouvelle->getImage())) {?>
+        <img src="<?= $nouvelle->getImage()?>" alt="riplimage">
+      <?php } ?>
+    </div>
     <br>
-    <a href="<?= $nouvelle->getUrl()?>"> Article complet </a>
   </body>
 </html>
